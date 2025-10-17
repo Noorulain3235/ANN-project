7ac8b2fe8efff7b7837fc2a03175bc539cfb3bd0
Nooroorulain3235 <Noorulain3235@gmail.com>
2025-10-16 23:56:44 -0700

    fix: cleaned vite.config and tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 
 import Header from "./container/header";
 import Home from "./components/home";
